b593ab45409689dc351c49d0dc2be49c7b49c00b
Terry Chia <terrycwk1994@gmail.com>
2015-08-16 14:39:50 +0800

    Remove use of sudo in TravisCI.

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,12 +3,7 @@
 set -e
 set -x
 
-sudo add-apt-repository -y ppa:fkrull/deadsnakes
-sudo apt-get -y update
-
-sudo apt-get install python3.3 python3.3-dev
-
-sudo pip install virtualenv
+pip install virtualenv
 
 virtualenv ~/.venv
 source ~/.venv/bin/activate
