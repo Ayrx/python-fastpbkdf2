8d91b1b53194ccdb1ce8e0148e4b88e640083598
Terry Chia <terrycwk1994@gmail.com>
2015-08-16 18:59:48 +0800

    Move algorithm lookup table outside of function.

--- src/fastpbkdf2/__init__.py
+++ src/fastpbkdf2/__init__.py
@@ -7,15 +7,16 @@ from __future__ import absolute_import, division, print_function
 from fastpbkdf2._fastpbkdf2 import ffi, lib
 
 
+algorithm = {
+    "sha1": (lib.fastpbkdf2_hmac_sha1, 20),
+    "sha256": (lib.fastpbkdf2_hmac_sha256, 32),
+    "sha512": (lib.fastpbkdf2_hmac_sha512, 64),
+}
+
+
 def pbkdf2_hmac(name, password, salt, rounds, dklen=None):
     if name not in ["sha1", "sha256", "sha512"]:
         raise ValueError("unsupported hash type")
-
-    algorithm = {
-        "sha1": (lib.fastpbkdf2_hmac_sha1, 20),
-        "sha256": (lib.fastpbkdf2_hmac_sha256, 32),
-        "sha512": (lib.fastpbkdf2_hmac_sha512, 64),
-    }
 
     out_length = dklen or algorithm[name][1]
     out = ffi.new("uint8_t[]", out_length)
